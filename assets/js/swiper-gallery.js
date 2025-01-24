8a9f8b58cbdd84c4f347ee223069a6973cecbbfd
sinashakiba13 <shakibasina75@gmail.com>
2025-01-24 18:35:18 +0330

    =Debug Swiper Sliders

--- assets/js/swiper-gallery.js
+++ assets/js/swiper-gallery.js
@@ -1,18 +1,25 @@
-var swiper = new Swiper(".mySwiper", {
+const all_swipers = document.querySelectorAll(".mySwiper")
+const all_swipers2 = document.querySelectorAll(".mySwiper2")
+const galleries = document.querySelectorAll(".gallery .card")
+
+galleries.forEach(e => {
+  var swiper = new Swiper(e.querySelector(".mySwiper"), {
     loop: true,
     spaceBetween: 10,
     slidesPerView: 4,
     freeMode: true,
     watchSlidesProgress: true,
   });
-  var swiper2 = new Swiper(".mySwiper2", {
-    loop: true,
-    spaceBetween: 10,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    thumbs: {
-      swiper: swiper,
-    },
-  });
+
+    var swiper2 = new Swiper(e.querySelector(".mySwiper2"), {
+      loop: true,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+});
